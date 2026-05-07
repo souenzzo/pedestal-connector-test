b3d549e5ea13815d9f65c77914388e5313aa19eb
Enzzo Cavallo <souenzzo@gmail.com>
2026-05-07 16:53:17 -0300

    Support jdk11

--- src/pedestal_connector_test/api.clj
+++ src/pedestal_connector_test/api.clj
@@ -1,7 +1,8 @@
 (ns pedestal-connector-test.api
   (:require [clojure.string :as string]
             [io.pedestal.connector :as conn])
-  (:import (java.net URI)
+  (:import (java.lang AutoCloseable)
+           (java.net URI)
            (java.net.http HttpClient HttpClient$Version HttpHeaders HttpRequest HttpResponse$BodyHandlers)
            (java.time Duration)
            (java.util Optional)))
@@ -38,33 +39,36 @@
                (create-connector {})
                conn/start!)]
     (try
-      (with-open [http-client (HttpClient/newHttpClient)]
-        (let [http-response (.send http-client
-                              (proxy [HttpRequest] []
-                                (method [] (-> request-method name string/upper-case))
-                                (timeout [] (Optional/of (Duration/ofSeconds 1)))
-                                (expectContinue [] false)
-                                (bodyPublisher [] (if (contains? ring-request :body)
-                                                    (Optional/of body)
-                                                    (Optional/empty)))
-                                (version [] (if (contains? ring-request :protocol)
-                                              (Optional/of
-                                                (case protocol
-                                                  "HTTP/1.1" HttpClient$Version/HTTP_1_1
-                                                  "HTTP/2.0" HttpClient$Version/HTTP_2))
-                                              (Optional/empty)))
-                                (headers [] (HttpHeaders/of headers
-                                              (constantly true)))
-                                (uri [] (URI. "http" nil "0" port uri query-string nil)))
-                              response-body-handler)]
-          {:body    (.body http-response)
-           :headers (into {}
-                      (map (fn [[k vs]]
-                             [k (if (next vs)
-                                  (vec vs)
-                                  (first vs))]))
-                      (.map (.headers http-response)))
-           :status  (.statusCode http-response)}))
+      (let [http-client (HttpClient/newHttpClient)]
+        (try
+          (let [http-response (.send http-client
+                                (proxy [HttpRequest] []
+                                  (method [] (-> request-method name string/upper-case))
+                                  (timeout [] (Optional/of (Duration/ofSeconds 1)))
+                                  (expectContinue [] false)
+                                  (bodyPublisher [] (if (contains? ring-request :body)
+                                                      (Optional/of body)
+                                                      (Optional/empty)))
+                                  (version [] (if (contains? ring-request :protocol)
+                                                (Optional/of
+                                                  (case protocol
+                                                    "HTTP/1.1" HttpClient$Version/HTTP_1_1
+                                                    "HTTP/2.0" HttpClient$Version/HTTP_2))
+                                                (Optional/empty)))
+                                  (headers [] (HttpHeaders/of headers
+                                                (constantly true)))
+                                  (uri [] (URI. "http" nil "0" port uri query-string nil)))
+                                response-body-handler)]
+            {:body    (.body http-response)
+             :headers (into {}
+                        (map (fn [[k vs]]
+                               [k (if (next vs)
+                                    (vec vs)
+                                    (first vs))]))
+                        (.map (.headers http-response)))
+             :status  (.statusCode http-response)})
+          (when (instance? AutoCloseable http-client)
+            (.close ^AutoCloseable http-client))))
       (finally
         (conn/stop! conn)))))
 
